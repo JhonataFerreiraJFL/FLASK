1a5e68bee9e3a69d6f3e5e4ecdbe0828e31489ba
Jhonata Ferreira <118383368+teamdevdigimap@users.noreply.github.com>
2024-08-12 23:14:10 -0300

    login de usuario

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -9,9 +9,15 @@ def index():
     return render_template(
         'index.html',
         )
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    print(form.username.data)
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
     return render_template('login.html',
                            form=form
                            )
